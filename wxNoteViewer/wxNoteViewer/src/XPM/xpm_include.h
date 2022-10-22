daa3debbeb2edd7b5ecc3c55e0fb8872209512b0
0x413 <jovenreffo@gmail.com>
2022-10-22 10:00:12 -0600

    - XPM include file

---
+++ wxNoteViewer/wxNoteViewer/src/XPM/xpm_include.h
@@ -0,0 +1,12 @@
+#ifndef _XPMINCLUDE_H_
+#define _XPMINCLUDE_H_
+
+#include "cut.xpm"
+#include "copy.xpm"
+#include "paste.xpm"
+#include "undo.xpm"
+#include "redo.xpm"
+#include "zoomin.xpm"
+#include "zoomout.xpm"
+
+#endif
