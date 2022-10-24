d583a2f7b0feed6d800c33ebb7246ef1f93fa666
0x413 <jovenreffo@gmail.com>
2022-10-24 06:47:10 -0600

    - events, members, IDs

--- wxNoteViewer/wxNoteViewer/src/NoteViewer.h
+++ wxNoteViewer/wxNoteViewer/src/NoteViewer.h
@@ -3,6 +3,7 @@
 
 #include <wx/wx.h>
 #include <wx/textctrl.h>
+#include <wx/sizer.h>
 
 #define WINDOW_SIZE (wxSize(670, 500))
 #define WINDOW_STYLE (wxDEFAULT_FRAME_STYLE)
@@ -12,13 +13,16 @@ enum
 	ID_CUT,
 	ID_COPY,
 	ID_PASTE,
+	ID_SELECT_ALL,
 	ID_UNDO,
 	ID_REDO,
 	ID_ZOOMIN,
 	ID_ZOOMOUT,
 	ID_OPEN,
 	ID_SAVE,
-	ID_SAVEAS
+	ID_SAVEAS,
+	ID_CLEAR_TEXT,
+	ID_CHANGE_FONT
 };
 
 class NoteViewer: public wxFrame
@@ -26,7 +30,20 @@ class NoteViewer: public wxFrame
 	DECLARE_EVENT_TABLE()
 
 private:
+	wxString m_currentFileName;
+	wxString m_currentFilePath;
+	wxString m_saveFilePath;
+
+	bool m_bCurrentFile{ false };
+	bool m_bFileSaved{ false };
+
+	wxBoxSizer* m_topSizer;
+	wxBoxSizer* m_buttonSizer;
+	
 	wxTextCtrl* m_pTextBox;
+	wxButton* m_clearButton;
+	wxButton* m_fontButton;
+
 	wxToolBar* m_pToolBar;
 	wxMenuBar* m_pMenuBar;
 	wxMenu* m_pFileMenu;
@@ -51,15 +68,19 @@ public:
 	void SetupBitmaps();
 	void SetupToolBar();
 	void SetupMenuBar();
+	void SetupControls();
+	void SetupSizers();
 
 	// Events
 	void OnExit(wxCommandEvent& event);
+	void OnText(wxCommandEvent& event);
 	void OnOpen(wxCommandEvent& event);
 	void OnSave(wxCommandEvent& event);
 	void OnSaveAs(wxCommandEvent& event);
 	void OnCut(wxCommandEvent& event);
 	void OnCopy(wxCommandEvent& event);
 	void OnPaste(wxCommandEvent& event);
+	void OnSelectAll(wxCommandEvent& event);
 	void OnUndo(wxCommandEvent& event);
 	void OnRedo(wxCommandEvent& event);
 	void OnZoomIn(wxCommandEvent& event);
